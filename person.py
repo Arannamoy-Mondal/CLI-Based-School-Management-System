6a9a5d803b177f34835ec18363cbdf87eb863f5f
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-10-19 01:54:24 +0600

    Getter setter method

---
+++ person.py
@@ -0,0 +1,35 @@
+import random
+from school import School
+class Person:
+    def __init__(self,name):
+        self.name=name
+
+class Teacher(Person):
+    def __init__(self, name):
+        super().__init__(name)
+    def evaluate_exam(self):
+        return random.randrange(1,100)
+    
+class Student(Person):
+    def __init__(self, name,classroom):
+        super().__init__(name)
+        self.clasroom=classroom
+        self.__id=None
+        self.marks={}
+        self.subject_grade={}
+        self.grade={}
+
+    def final_grade(self):
+        sum=0
+        for grade in self.subject_grade.values():
+            sum+=School.grade_to_value(grade)
+        gpa=sum/len(self.subject_grade)
+        self.grade=School.value_to_grade(gpa)
+
+    @property # getter method
+    def id(self):
+        return self.__id
+    
+    @id.setter
+    def id(self,id):
+        self.id=id
---
+++ test.py
@@ -0,0 +1,5 @@
+map={
+    'A':50,'B':60,'C':80
+}
+for i in map.keys():
+    print(i)
